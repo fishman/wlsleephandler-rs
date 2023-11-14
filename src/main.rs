f68f2128aeeff0cbe11be144071b6eab4f9a8e4b
Reza Jelveh <git@jelveh.me>
2023-11-14 14:38:58 +0800

    mlua: enable sandboxing and add UserData for adding functions

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use clap::Parser;
-use mlua::Lua;
+use mlua::{Lua, UserData, UserDataMethods, Function};
 use tokio::sync::mpsc;
 use wayrs_protocols::ext_idle_notify_v1::{ExtIdleNotificationV1, ExtIdleNotifierV1};
 use std::{fs, print};
@@ -9,14 +9,17 @@ use wayrs_client::protocol::wl_seat::WlSeat;
 use wayrs_client::Connection;
 use wayrs_client::EventCtx;
 
+// mod idle_notifier;
+
 #[derive(Debug)]
 pub enum Request {
     SetTimeout(i32),
 }
 
 #[derive(Debug)]
-struct State {
-    idle_notification: ExtIdleNotificationV1,
+struct SharedState {
+    idle_notifier: ExtIdleNotifierV1,
+    idle_notifications: Vec<ExtIdleNotificationV1>,
 }
 
 
@@ -40,11 +43,16 @@ pub async fn run(mut rx: mpsc::Receiver<Request>) -> Result<(), Box<dyn std::err
         })
         .expect("idle_notifier not found");
 
-    let idle_notification = idle_notifier.get_idle_notification_with_cb(
+    let mut state = SharedState {
+        idle_notifier,
+        idle_notifications: Vec::new(),
+    };
+
+    let idle_notification = state.idle_notifier.get_idle_notification_with_cb(
         &mut conn, 
         10000, 
         seat, 
-        |ctx: EventCtx<State, ExtIdleNotificationV1>| {
+        |ctx: EventCtx<SharedState, ExtIdleNotificationV1>| {
             match ctx.event {
                 ext_idle_notification_v1::Event::Idled => {
                     println!("Idled");
@@ -57,20 +65,7 @@ pub async fn run(mut rx: mpsc::Receiver<Request>) -> Result<(), Box<dyn std::err
         }
     );
 
-    for global in globals {
-        println!(
-            "{} v{}",
-            global.interface.into_string().unwrap(),
-            global.version
-        );
-    }
-
-    //
-    let mut state = State {
-        idle_notification: idle_notification
-    };
-    //
-
+    state.idle_notifications.push(idle_notification);
 
     loop {
         conn.async_flush().await?;
@@ -87,6 +82,35 @@ pub async fn run(mut rx: mpsc::Receiver<Request>) -> Result<(), Box<dyn std::err
     }
 }
 
+fn idle_notification_cb(lua_cb: Function, ctx: EventCtx<SharedState, ExtIdleNotificationV1>) -> mlua::Result<()> {
+    // You will need to map the Rust event context to a format that Lua can understand.
+    // This example assumes you've created a way to map `ctx.event` to a Lua value.
+    // let event_value = map_event_ctx_to_lua_value(ctx)?;
+    //
+    // // Call the Lua function with the event value.
+    // lua_cb.call::<_, ()>(event_value)?;
+    Ok(())
+}
+
+struct MyLuaFunctions;
+
+impl UserData for MyLuaFunctions {
+    fn add_methods<'lua, M: UserDataMethods<'lua, Self>>(methods: &mut M) {
+        methods.add_function("idle_notification", |lua, (timeout, lua_cb): (i32, Function)| {
+            // let idle_notifier = // Initialize or retrieve your idle_notifier instance
+
+            // let _idle_notification = idle_notifier.get_idle_notification_with_cb(
+            //     &mut conn, 
+            //     timeout, 
+            //     seat, 
+            //     move |ctx| idle_notification_cb(lua_cb.clone(), ctx)
+            // );
+
+            Ok(())
+        });
+    }
+}
+
 #[derive(Parser, Debug)]
 #[command(author, version, about, long_about = None)]
 struct Args {
@@ -97,12 +121,13 @@ struct Args {
 #[tokio::main]
 async fn main() -> mlua::Result<()> {
 // fn main() -> mlua::Result<()> {
-    // let (_tx, rx) = mpsc::channel::<T>(32);
-    // let (mut tx, mut rx) = mpsc::channel(32);
+    let (_tx, rx) = mpsc::channel(32);
 
     let args = Args::parse();
 
+    // let shared_state = Arc::new(Mutex::new(SharedState { counter: 0 }));
     let lua = Lua::new();
+    lua.sandbox(true)?;
 
     let config_path = args.config;
     let config_script = fs::read_to_string(config_path)?;
@@ -121,17 +146,15 @@ async fn main() -> mlua::Result<()> {
     //     .unwrap();
     //
     //
-    use tokio::time::{sleep, Duration};
-    let (_tx, rx) = mpsc::channel(16);
 
-    run(rx).await.map_err(|e| mlua::Error::external(format!("An unknown error occurred: {}", e)))?;
+    let handle = tokio::spawn(async{
+        let _ = run(rx).await.map_err(|e| mlua::Error::external(format!("An unknown error occurred: {}", e)));
+    });
 
-    // tokio::task::spawn(async move {
-    //     loop {
-    //         // Handle Wayland events
-    //         // The call to roundtrip is a blocking call
-    //     }
-    // });
+    match handle.await {
+        Ok(_) => println!("Task completed successfully"),
+        Err(e) => println!("Task failed: {:?}", e),
+    }
 
     Ok(())
 }
