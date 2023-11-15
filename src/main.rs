ca9d9ae6141c09106529058a018e811dfb372789
Reza Jelveh <git@jelveh.me>
2023-11-16 00:12:15 +0800

    experiment with wayland_client

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,35 @@
 use clap::Parser;
 use mlua::{Lua, UserData, UserDataMethods, Function};
 use tokio::sync::mpsc;
-use wayrs_protocols::ext_idle_notify_v1::{ExtIdleNotificationV1, ExtIdleNotifierV1};
 use std::{fs, print};
 use std::sync::{Arc, Mutex};
+use wayland_client::{Connection, Dispatch, QueueHandle, EventQueue};
+use wayland_client::protocol::{wl_registry, wl_seat};
+use  wayland_protocols::ext::idle_notify::v1::client::{ext_idle_notifier_v1, ext_idle_notification_v1};
+use tokio::task;
+use std::task::{Poll, Context};
+use std::pin::Pin;
+use std::convert::Infallible;
 
-use wayrs_protocols::ext_idle_notify_v1::*;
-use wayrs_client::protocol::wl_seat::WlSeat;
-use wayrs_client::Connection;
-use wayrs_client::EventCtx;
+// fn my_async_function(event_handler: &mut EventQueue<State>, qhandle: QueueHandle<State>, state: State) {
+//     let mut context = Context::from_waker(futures::task::noop_waker_ref());
+//
+//     loop {
+//         match Pin::new(&mut event_handler).poll_dispatch_pending(&mut context, &mut state) {
+//             // Poll::Ready(Infallible::from(Ok(())) => {
+//             //     // All pending events have been processed
+//             //     // return Ok::<(), Infallible>(());
+//             // },
+//             Poll::Ready(Err(e)) => {
+//                 // Handle errors here
+//             },
+//             Poll::Pending => {
+//                 // No events to process, can yield to other tasks
+//                 break;
+//             }
+//         }
+//     }
+// }
 
 // mod idle_notifier;
 
@@ -16,214 +37,6 @@ use wayrs_client::EventCtx;
 pub enum Request {
     Reload,
 }
-
-#[derive(Debug)]
-struct State {
-    idle_notifier: ExtIdleNotifierV1,
-    idle_notifications: Vec<ExtIdleNotificationV1>,
-}
-
-#[derive(Debug)]
-pub struct LuaState {
-    lua: Lua,
-}
-
-struct LuaStateNew { }
-
-type LuaStateHandle = Arc<Mutex<LuaState>>;
-
-
-fn lua_init(lua: &Lua, tx: mpsc::Sender<i32>) -> Result<(), Box<dyn std::error::Error>> {
-    lua.sandbox(true)?;
-    let args = Args::parse();
-
-    let config_path = args.config;
-    lua.globals().set("get_idle_notification", create_idle_notification_fn(lua, tx.clone())?)?;
-    let config_script = fs::read_to_string(config_path)?;
-    let config: mlua::Table = lua.load(&config_script).eval()?; 
-
-    // Retrieve settings from Lua config
-    let _idle_duration = config.get::<_, u64>("idle_duration")?;
-    let _idle_script = config.get::<_, String>("idle_script")?;
-
-    print!("idle_duration = {}\n", _idle_duration);
-
-    Ok(())
-}
-
-// pub fn create_idle_notification(
-//     idle_notifier: ExtIdleNotifierV1,
-//     conn: &mut Connection<State>,
-//     seat: WlSeat) -> ExtIdleNotificationV1 {
-//     return idle_notifier.get_idle_notification_with_cb(
-//         conn, 
-//         10000, 
-//         seat, 
-//         |ctx: EventCtx<State, ExtIdleNotificationV1>| {
-//             match ctx.event {
-//                 ext_idle_notification_v1::Event::Idled => {
-//                     println!("Idled");
-//                 }
-//                 ext_idle_notification_v1::Event::Resumed => {
-//                     println!("Resumed");
-//                 }
-//                 _ => {}
-//             }
-//         }
-//     );
-// }
-
-pub async fn run(mut rx: mpsc::Receiver<Request>) -> Result<(), Box<dyn std::error::Error>> {
-    let (mut conn, globals) = Connection::async_connect_and_collect_globals().await?;
-
-
-    {
-        // let lua = &shared_state.lock().unwrap().lua;
-
-        let lua = Lua::new();
-        let _ = lua_init(&lua);
-    }
-
-
-    let registry = conn.registry();
-    let seat: WlSeat = globals.iter()
-        .find(|global| global.interface.to_str().unwrap() == "wl_seat")
-        .map(|seat_global| {
-            // Bind to the wl_seat
-            registry.bind(&mut conn, seat_global.name, seat_global.version)
-        })
-        .expect("wl_seat not found");
-
-    let idle_notifier: ExtIdleNotifierV1 = globals.iter()
-        .find(|global| global.interface.to_str().unwrap() == "ext_idle_notifier_v1")
-        .map(|idle_notifier_global| {
-            // Bind to the wl_seat
-            registry.bind(&mut conn, idle_notifier_global.name, idle_notifier_global.version)
-        })
-        .expect("idle_notifier not found");
-
-    let mut state = State {
-        idle_notifier,
-        idle_notifications: Vec::new(),
-    };
-
-    // let idle_notification = create_idle_notification(state.idle_notifier, conn, seat);
-    let idle_notification = idle_notifier.get_idle_notification_with_cb(
-        &mut conn, 
-        10000, 
-        seat, 
-        |ctx: EventCtx<State, ExtIdleNotificationV1>| {
-            match ctx.event {
-                ext_idle_notification_v1::Event::Idled => {
-                    println!("Idled");
-                }
-                ext_idle_notification_v1::Event::Resumed => {
-                    println!("Resumed");
-                }
-                _ => {}
-            }
-        }
-    );
-
-    state.idle_notifications.push(idle_notification);
-
-    loop {
-        conn.async_flush().await?;
-
-        tokio::select! {
-            recv_events = conn.async_recv_events() => {
-                recv_events?;
-                conn.dispatch_events(&mut state);
-            }
-            Some(request) = rx.recv() => {
-                match request {
-                    Request::Reload => {
-                        println!("Reloading config");
-                    }
-                }
-            }
-        }
-    }
-}
-
-
-struct SenderWrapper(mpsc::Sender<i32>);
-    // shared_state: LuaStateHandle,
-
-impl UserData for SenderWrapper {}
-
-pub async fn create_idle_notification() -> Result<(), Box<dyn std::error::Error>> {
-    let (mut conn, globals) = Connection::async_connect_and_collect_globals().await?;
-
-
-    let registry = conn.registry();
-    let seat: WlSeat = globals.iter()
-        .find(|global| global.interface.to_str().unwrap() == "wl_seat")
-        .map(|seat_global| {
-            // Bind to the wl_seat
-            registry.bind(&mut conn, seat_global.name, seat_global.version)
-        })
-        .expect("wl_seat not found");
-
-    let idle_notifier: ExtIdleNotifierV1 = globals.iter()
-        .find(|global| global.interface.to_str().unwrap() == "ext_idle_notifier_v1")
-        .map(|idle_notifier_global| {
-            // Bind to the wl_seat
-            registry.bind(&mut conn, idle_notifier_global.name, idle_notifier_global.version)
-        })
-        .expect("idle_notifier not found");
-
-    let mut state = LuaStateNew { };
-
-    loop {
-        conn.async_flush().await?;
-
-        tokio::select! {
-            recv_events = conn.async_recv_events() => {
-                recv_events?;
-                conn.dispatch_events(&mut state);
-            }
-        }
-    }
-}
-
-fn idle_notification_cb(lua_cb: Function, ctx: EventCtx<State, ExtIdleNotificationV1>) -> mlua::Result<()> {
-    // You will need to map the Rust event context to a format that Lua can understand.
-    // This example assumes you've created a way to map `ctx.event` to a Lua value.
-    // let event_value = map_event_ctx_to_lua_value(ctx)?;
-    //
-    // // Call the Lua function with the event value.
-    // lua_cb.call::<_, ()>(event_value)?;
-    Ok(())
-}
-
-fn create_idle_notification_fn(lua: &Lua, tx: mpsc::Sender<i32>) -> mlua::Result<Function> {
-    let sender = SenderWrapper(tx);
-
-    lua.create_function(move |_, notification: String| {
-        let _sender = sender.0.clone();
-        tokio::spawn(async move {
-            let _ = create_idle_notification().await.unwrap();
-            // sender.send(message).await.unwrap();
-        });
-        Ok(())
-    })
-}
-    // fn add_methods<'lua, M: UserDataMethods<'lua, Self>>(methods: &mut M) {
-    //     methods.add_function("idle_notification", |lua, (timeout, lua_cb): (i32, Function)| {
-            // let idle_notifier = // Initialize or retrieve your idle_notifier instance
-
-            // let _idle_notification = idle_notifier.get_idle_notification_with_cb(
-            //     &mut conn, 
-            //     timeout, 
-            //     seat, 
-            //     move |ctx| idle_notification_cb(lua_cb.clone(), ctx)
-            // );
-
-//             Ok(())
-//         });
-//     }
-// }
 
 #[derive(Parser, Debug)]
 #[command(author, version, about, long_about = None)]
@@ -234,33 +47,93 @@ struct Args {
 
 #[tokio::main]
 async fn main() -> mlua::Result<()> {
-// fn main() -> mlua::Result<()> {
-    let (_tx, rx) = mpsc::channel(32);
+    let conn = Connection::connect_to_env().unwrap();
+    let mut event_queue: EventQueue<State> = conn.new_event_queue();
+    let qhandle = event_queue.handle();
 
+    let display = conn.display();
+    display.get_registry(&qhandle, ());
 
-    // let shared_state = Arc::new(Mutex::new(LuaState {
-    //     lua: Lua::new(),
-    // }));
-    //
+    let mut state = State {
+        wl_seat: None,
+        idle_notifier: None,
+    };
 
-    // let lua = &shared_state.lock().unwrap().lua;
-    // lua.sandbox(true)?;
+    // Run the event loop in a separate async task
+    // task::spawn(async move {
+    //     loop {
+    //         let _ = event_queue.dispatch_pending(&mut state).unwrap();
+    //         tokio::time::sleep(std::time::Duration::from_millis(10)).await;
+    //     }
+    // });
 
-    // let rt = runtime::Builder::new_multi_thread()
-    //     .enable_all()
-    //     .build()
-    //     .unwrap();
-    //
-    //
+    println!("Starting the example window app, press <ESC> to quit.");
 
-    let handle = tokio::spawn(async{
-        let _ = run(rx).await.map_err(|e| mlua::Error::external(format!("An unknown error occurred: {}", e)));
-    });
-
-    match handle.await {
-        Ok(_) => println!("Task completed successfully"),
-        Err(e) => println!("Task failed: {:?}", e),
-    }
+    // while true {
+    //     event_queue.blocking_dispatch(&mut state).unwrap();
+    // }
+    // Your application logic goes here
+    // ...
 
     Ok(())
 }
+
+struct State {
+    wl_seat: Option<wl_seat::WlSeat>,
+    idle_notifier: Option<ext_idle_notifier_v1::ExtIdleNotifierV1>,
+}
+
+impl Dispatch<wl_registry::WlRegistry, ()> for State {
+    fn event(
+        _: &mut Self,
+        registry: &wl_registry::WlRegistry,
+        event: wl_registry::Event,
+        _: &(),
+        _: &Connection,
+        qh: &QueueHandle<Self>,
+    ) {
+        if let wl_registry::Event::Global { name, interface, .. } = event {
+            println!("Global: {:?}", interface);
+            match &interface[..] {
+                "wl_seat" => {
+                    registry.bind::<wl_seat::WlSeat, _, _>(name, 1, qh, ());
+                    println!("Seat: {:?}", name);
+                }
+                "ext_idle_notifier_v1" => {
+                    println!("idle: {:?}", name);
+                    registry.bind::<ext_idle_notifier_v1::ExtIdleNotifierV1, _, _>(name, 1, qh, ());
+                }
+                _ => {}
+            }
+        }
+    }
+}
+
+impl Dispatch<wl_seat::WlSeat, ()> for State {
+    fn event(
+        state: &mut Self,
+        seat: &wl_seat::WlSeat,
+        _event: wl_seat::Event,
+        _: &(),
+        _: &Connection,
+        _qh: &QueueHandle<Self>,
+    ) {
+        state.wl_seat = Some(seat.clone());
+        println!("Seat:");
+    }
+}
+
+impl Dispatch<ext_idle_notifier_v1::ExtIdleNotifierV1, ()> for State {
+    fn event(
+        state: &mut Self,
+        idle_notifier: &ext_idle_notifier_v1::ExtIdleNotifierV1,
+        event: ext_idle_notifier_v1::Event,
+        _: &(),
+        _: &Connection,
+        _qh: &QueueHandle<Self>,
+    ) {
+        println!("Idle Notifier: {:?}", event);
+        state.idle_notifier = Some(idle_notifier.clone());
+    }
+}
+
