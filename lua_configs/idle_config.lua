b8fab3af47b10980e3a9c0e753f630150a47fff9
Reza Jelveh <git@jelveh.me>
2024-10-12 16:18:06 +0800

    update config

--- lua_configs/idle_config.lua
+++ lua_configs/idle_config.lua
@@ -1,67 +1,78 @@
 Helpers:log("Loading idle_config.lua")
 
 function LockScreen()
-  Helpers:log("Locking Screen")
-  IdleNotifier:run_once("swaylock -f")
+	Helpers:log("Locking Screen")
+	IdleNotifier:run_once("swaylock")
 end
 
 function LockHandler()
-  IdleNotifier:run("playerctl -a pause")
-  LockScreen()
+	IdleNotifier:run("playerctl -a pause")
+	IdleNotifier:run("nmcli radio wwan off")
+	LockScreen()
+end
+
+function UnlockHandler()
+	DpmsOn()
 end
 
 function DpmsOn()
-  Helpers:log("Turning screen on")
-  IdleNotifier:run("swaymsg output '*' dpms on")
-  IdleNotifier:run("hyprctl dispatch dpms on")
+	Helpers:log("Turning screen on")
+	IdleNotifier:run("swaymsg output '*' dpms on")
+	-- IdleNotifier:run("hyprctl dispatch dpms on")
 end
 
 function DpmsOff()
-  Helpers:log("Turning screen off")
-  IdleNotifier:run("swaymsg output '*' dpms off")
-  IdleNotifier:run("hyprctl dispatch dpms off")
+	Helpers:log("Turning screen off")
+	IdleNotifier:run("swaymsg output '*' dpms off")
+	-- IdleNotifier:run("hyprctl dispatch dpms off")
 end
 
 function HybridSleep()
-  IdleNotifier:run("systemctl hybrid-sleep")
+	IdleNotifier:run("systemctl suspend")
 end
 
 function ScreenLockBattery(event)
-  if event == "idled" and Helpers:on_battery() then
-    LockScreen()
-  end
+	if event == "idled" and Helpers:on_battery() then
+		LockScreen()
+	end
 end
 
 function SleepBattery(event)
-  if event == "idled" and Helpers:on_battery() then
-    HybridSleep()
-  end
+	if event == "idled" and Helpers:on_battery() then
+		HybridSleep()
+	end
+end
+
+function SleepAC(event)
+	if event == "idled" and Helpers:on_battery() then
+		HybridSleep()
+	end
 end
 
 function ScreenDpmsBattery(event)
-  -- Resumed should always be executed
-  if event == "resumed" then
-    DpmsOn()
-  elseif event == "idled" and Helpers:on_battery() then
-    DpmsOff()
-  end
+	-- Resumed should always be executed
+	if event == "resumed" then
+		DpmsOn()
+	elseif event == "idled" and Helpers:on_battery() then
+		DpmsOff()
+	end
 end
 
 function ScreenLockAC(event)
-  if event == "idled" and not Helpers:on_battery() then
-    LockScreen()
-  end
+	if event == "idled" and not Helpers:on_battery() then
+		LockScreen()
+	end
 end
 
 function ScreenDpmsAC(event)
-  if event == "resumed" then
-    DpmsOn()
-  elseif event == "idled" and not Helpers:on_battery() then
-    DpmsOff()
-  end
+	if event == "resumed" then
+		DpmsOn()
+	elseif event == "idled" and not Helpers:on_battery() then
+		DpmsOff()
+	end
 end
 
-DbusHandler:PrepareSleep("LockScreen")
+DbusHandler:PrepareSleep("LockHandler")
 DbusHandler:LockHandler("LockHandler")
 -- DbusHandler:UnlockHandler("UnlockHandler")
 IdleNotifier:get_notification(300,  "ScreenLockBattery")
